dfa2720ef2da4f5eb9ba89d4ee0447d1170420c9
santosst3 <stefanthiago@gmail.com>
2023-10-26 15:21:44 -0300

    Versao recursiva

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,7 @@
+def calc_fibo(obj_fibo):
+    obj_fibo.n_current = obj_fibo.calc_fibo_recursive(obj_fibo.n_max)
+
+
 class fibonacci_number:
     def __init__(self,nmax):
         self.n_ant = 0
@@ -9,15 +13,21 @@ class fibonacci_number:
     def __repr__(self):
         return f'Numero {self.n_max} da sequencia de Fibonacci: {self.n_current}'
     
-    def calc_fibo(self):
-        n = 1
-        while n < self.n_max:
-            aux = self.n_current
-            self.n_current += self.n_ant
-            self.n_ant = aux
-            n += 1
+    def calc_fibo_recursive(self,n):
+        if n > 1:
+            return self.calc_fibo_recursive(n-1) + self.calc_fibo_recursive(n-2)
+        return n+1
+        
+    
+#    def calc_fibo(self):
+#        n = 1
+#        while n < self.n_max:
+#            aux = self.n_current
+#            self.n_current += self.n_ant
+#            self.n_ant = aux
+#            n += 1
 
 
 fib1 = fibonacci_number(10)
-fib1.calc_fibo()
+calc_fibo(fib1)
 print(fib1)
